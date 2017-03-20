905a8214570b16d63089a39bc5aca0fcc76f6dfb
minhncsocial <minhnc.social@gmail.com>
2017-03-20 10:03:45 +0800

    comlete simple program test detection & display

--- detection/myselfApplication/detectionTestingProgram.m
+++ detection/myselfApplication/detectionTestingProgram.m
@@ -1,1 +1,27 @@
+%% initialize
+path_detectionUtils = 'E:\WORK\ORGANIZATION\NTUT\Robot Grasping\Project02\Code\deepGraspingCode1\detection\detectionUtils';
+path_utils = 'E:\WORK\ORGANIZATION\NTUT\Robot Grasping\Project02\Code\deepGraspingCode1\util';
+addpath(sprintf(path_detectionUtils,pwd));
+addpath(sprintf(path_utils,pwd));
 
+dataDir = 'E:\WORK\ORGANIZATION\NTUT\Robot Grasping\Project02\Code\rawDataSet';
+bgrDir = 'E:\WORK\ORGANIZATION\NTUT\Robot Grasping\Project02\Code\rawDataSet';
+instNum = sscanf('pcd0100r.png', '%*3c%u')
+
+load ../../data/bgNums.mat
+load ../../data/graspModes24.mat
+load ../../weights/graspWFinal.mat
+load ../../data/graspWhtParams.mat
+
+% Just use the positive-class weights for grasping.
+w_class = w_class(:,1);
+
+bgFN = sprintf('%s/pcdb%04dr.png',bgrDir,bgNo(instNum));
+
+rotAngs = 0:15:(11*15);
+heights = 10:10:90;
+widths = 10:10:90;
+scanStep = 10;
+
+%% detect
+[bestRects,bestScores] = onePassDectionForInstDefaultParamsDisplay(101,dataDir,bgrDir)
